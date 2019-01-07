eaa390ca3444cb0f638d5d71ac7fdb98b5938939
Eddy Yao <eddy2yao2@gmail.com>
2019-01-07 13:49:06 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,16 @@ function changeLarge(){
 	}
 }
 
-$(window).on("hashchange", function () {
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
     window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+
+$(document).on('click', 'a[href^="#"]', function(event) {
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
 });
+
+window.setTimeout(offsetAnchor, 0);
